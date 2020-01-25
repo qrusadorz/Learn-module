486bf61a960e90b9947b16bfbe405a42562eec51
qrusadorz <qrusadorz@gmail.com>
2020-01-25 21:58:12 +0900

    add a display2 method

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const display = (text) => console.log(text);
+const display = (text) => console.log("display1:" + text);
+const display2 = (text) => console.log("display2:" + text);
 
 // display("test");
 
 // ESM syntax is supported.
 export {
-	display
+	display,
+	display2
 }
